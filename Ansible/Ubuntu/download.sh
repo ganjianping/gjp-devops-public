e964cf57bc96b187d940877838d900454a96df0f
ganjianping <ganjianping1984@gmail.com>
2026-02-15 19:12:15 +0800

    Add and update Ansible playbooks: Node.js multi-version install via NVM, simplified Nginx install from Ubuntu repo, and Nginx config download utility

---
+++ Ansible/Ubuntu/download.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ansible-playbook -i inventory/hosts playbook/nginx/download_nginx_config_files.yml
--- Ansible/Ubuntu/login.sh
+++ Ansible/Ubuntu/login.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -i ~/.ssh/id_rsa ubuntu@13.251.126.203
+ssh -i ~/.ssh/id_rsa ubuntu@13.214.159.84
